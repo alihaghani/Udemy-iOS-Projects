7e904d20d470314676f6b40ec0d0fd3741266230
Ali Haghani <alihaghani96@gmail.com>
2016-02-11 22:19:24 -0800

    Finished Current Condition weather app.

---
+++ Current Conditions/Current Conditions/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  Current Conditions
+//
+//  Created by Ali Haghani on 2016-02-11.
+//  Copyright © 2016 Ali Haghani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var cityName: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func checkWeather(sender: AnyObject) {
+        var weatherSummary = String()
+        var wasSuccessful = false
+        
+        let attemptedUrl = NSURL(string: "http://www.weather-forecast.com/locations/" +
+            cityName.text!.stringByReplacingOccurrencesOfString(" ", withString: "-").stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+            +
+            "/forecasts/latest")
+        
+        if let url = attemptedUrl {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+                if let urlContent = data {
+                    
+                    let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                    
+                    let websiteArray = webContent!.componentsSeparatedByString("Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                    
+                    
+                    if websiteArray.count > 1 {
+                        
+                        let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
+                        
+                        if weatherArray.count > 1 {
+                            
+                            wasSuccessful = true
+                            
+                            weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                            
+                        }
+                    }
+                    
+                    
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.resultLabel.text = weatherSummary
+                        
+                        if wasSuccessful == false {
+                            self.resultLabel.text = "Couldn't find the weather for that city, please try again."
+                        }
+                        
+                    })
+                    
+                    
+                }
+                
+            }
+            
+            
+            
+            task.resume()
+            
+        } else {
+            self.resultLabel.text = "Couldn't find the weather for that city, please try again."
+        }
+        
+        
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.cityName.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
+
