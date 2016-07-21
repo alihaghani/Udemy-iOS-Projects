a9d066da2620ebcd360c3d80ec25d95652d9c0c0
Ali Haghani <Ali@CHD-Mac-Pro.local>
2016-07-20 17:55:06 -0700

    Updated game logic.

--- Tic Tac Toe/Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var activePlayer = 1 // 1 = o, 2 = x
+    
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    
+    
+    @IBOutlet weak var button: UIButton!
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        if (gameState[sender.tag] == 0) {
+            
+            gameState[sender.tag] = activePlayer
+            
+            if activePlayer == 1 {
+                
+                sender.setImage(UIImage(named: "o.jpg"), forState: .Normal)
+                
+                activePlayer = 2
+                
+                
+            } else {
+                sender.setImage(UIImage(named: "x.png"), forState: .Normal)
+                
+                activePlayer = 1
+                
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
