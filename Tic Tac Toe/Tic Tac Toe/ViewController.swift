ec6fa50e23d9c863324b2ace65147f2188efc89b
Ali Haghani <Ali@CHD-Mac-Pro.local>
2016-07-27 17:48:48 -0700

    Added winning combinations.

--- Tic Tac Toe/Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     
+    let winningCombinations = [[0,1,2], [3,4,5], [6,7,8] ,[0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
     
     
     @IBOutlet weak var button: UIButton!
@@ -37,6 +38,20 @@ class ViewController: UIViewController {
                 
                 activePlayer = 1
                 
+            }
+            
+            for combination in winningCombinations {
+                if (gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]) {
+                    
+                    if gameState[combination[0]] == 1 {
+                        print("O has won!")
+                    }
+                    
+                    else {
+                        print("X has won!")
+                    }
+                    
+                }
             }
         }
         
